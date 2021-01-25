1b3e47958da28c86232cbc178fa180615072a288
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-01-25 19:58:22 +0330

    Update install.py

--- install.py
+++ install.py
@@ -2,4 +2,5 @@ import os
 os.system("pip install requests")
 os.system("pip install colorama")
 os.system("pkg install python")
-os.system("pkg install python")
+os.system("unzip Gmail-Bomber.zip")
+os.system("python gmail.py")
